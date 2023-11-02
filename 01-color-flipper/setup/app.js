d14925e79cb4fdded0361ea066ff913d9cb2fa12
Bruno Verçosa <bruno.nv@hotmail.com>
2023-11-01 21:49:35 -0300

    Exercicios 1 e 2

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,19 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const botao = document.getElementById("btn");
+const cor = document.querySelector(".color");
+
+botao.addEventListener("click", function(){
+    //obtém um número aleatório
+    const numeroAleatorio = getNumeroAletorio();
+
+    document.body.style.backgroundColor = 
+        colors[numeroAleatorio];
+        cor.textContent = colors[numeroAleatorio];
+});
+
+//Retorna um número inteiro aleatório entre 0 e o comprimento
+//do array colors
+function getNumeroAletorio(){
+    return Math.floor(Math.random() * colors.length);
+}
+
